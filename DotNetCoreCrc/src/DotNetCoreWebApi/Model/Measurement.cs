190d4df40096037136fa134182e2ab955ae729df
Badyl <pietrasiak.patryk.05@gmail.com>
2019-04-01 18:39:12 +0200

    added fields to measurements class

--- DotNetCoreCrc/src/DotNetCoreWebApi/Model/Measurement.cs
+++ DotNetCoreCrc/src/DotNetCoreWebApi/Model/Measurement.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,21 @@ namespace DotNetCoreWebApi.Model
 {
     public class Measurement
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(4,2)")]
+        public decimal Value { get; set; }
+
+        [Required]
+        public string CreatedBy { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
     }
 }
