699ce2d5d68a7eb4c44fff8e256bdb6e7ad2bca3
Badyl <pietrasiak.patryk.05@gmail.com>
2019-04-01 18:50:24 +0200

    added DbContext implementation to MeasurementContext class

--- DotNetCoreCrc/src/DotNetCoreWebApi/DbContexts/MeasurementContext.cs
+++ DotNetCoreCrc/src/DotNetCoreWebApi/DbContexts/MeasurementContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using DotNetCoreWebApi.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DotNetCoreWebApi.DbContexts
 {
-    public class MeasurementContext
+    public class MeasurementContext : DbContext
     {
+        public MeasurementContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Measurement> Measurments { get; set; }
     }
 }
