8418df23db69d366f7a9eefbbdaed2a2d129f6ea
Patryk Pietrasiak <pietrasiak.patryk.05@gmail.com>
2020-03-09 14:51:02 +0100

    Update IMeasurementRepository.cs
    sd

--- DotNetCoreCrc/src/DotNetCoreWebApi/Repository/IMeasurementRepository.cs
+++ DotNetCoreCrc/src/DotNetCoreWebApi/Repository/IMeasurementRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DotNetCoreWebApi.Repository
 {
-    public interface IMeasurement<TEntity>
+    public interface IMeasurementService<TEntity>
     {
         Task<IEnumerable<TEntity>> GetAll();
 
