7ea00cb20023fd91b796f106afd8621c0f8d8b37
Patryk Pietrasiak <pietrasiak.patryk.05@gmail.com>
2020-03-11 14:35:38 +0100

    Update MeasurementController.cs
    sd

--- DotNetCoreCrc/src/DotNetCoreWebApi/Controllers/MeasurementController.cs
+++ DotNetCoreCrc/src/DotNetCoreWebApi/Controllers/MeasurementController.cs
@@ -18,49 +18,37 @@ namespace DotNetCoreWebApi.Controllers
         {
             this._measurementRepository = measurementRepository;
         }
-
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             var measurements = await _measurementRepository.GetAll();
-
             return Ok(measurements);
         }
         [HttpGet("{id}", Name = "Get")]
         public async Task<IActionResult> Get(long id)
         {
             var measurements = await _measurementRepository.Get(id);
-
             return Ok(measurements);
         }
-
         [HttpPost]
         public async Task<IActionResult> Add(Measurement measurement)
         {
             await _measurementRepository.Add(measurement);
-
             return CreatedAtAction(nameof(Get), new { id = measurement.Id }, measurement);
-
         }
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(long id, Measurement measurement)
         {
             var measurementsToUpdate = await _measurementRepository.Get(id);
             await _measurementRepository.Update(measurementsToUpdate, measurement);
-
-
             return NoContent();
         }
-
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(long id)
         {
             var measurementToDelete = await _measurementRepository.Get(id);
             await _measurementRepository.Delete(measurementToDelete);
-
-
             return NoContent();
         }
-
     }
 }
